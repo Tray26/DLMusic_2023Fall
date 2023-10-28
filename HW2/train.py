97648e155e09225bec8c525f3740685952c71401
Tray26 <b08901177@ntu.edu.tw>
2023-10-28 21:06:07 +0800

    update gitignore

--- HW2/train.py
+++ HW2/train.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     parser.add_argument('--input_mels_dir', default='ft_dataset')
     parser.add_argument('--input_training_file', default='LJSpeech-1.1/training.txt')
     parser.add_argument('--input_validation_file', default='LJSpeech-1.1/validation.txt')
-    parser.add_argument('--checkpoint_path', default='cp_hifigan_self')
+    parser.add_argument('--checkpoint_path', default='./checkpoints/cp_hifigan_self')
     parser.add_argument('--config', default='')
     parser.add_argument('--training_epochs', default=3100, type=int)
     parser.add_argument('--stdout_interval', default=5, type=int)
